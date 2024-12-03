c28ecad262a2d28eff2a7532c5da846341fe6d74
Nermin Skenderovic <nesk@netcompany.com>
2024-12-03 08:45:41 +0100

    d02

---
+++ src/d02/ab.ts
@@ -0,0 +1,49 @@
+const fs = require('fs')
+
+const data: number[][] = fs
+  .readFileSync(__dirname + '/data.txt', 'utf8')
+  .split('\n')
+  .map((report: string) => report.split(' ').map(Number))
+
+const isSafe = (report: number[]) => {
+  let isIncreasing = true
+  let isDecreasing = true
+
+  for (let i = 1; i < report.length; i++) {
+    const diff = report[i] - report[i - 1]
+    const diffAbs = Math.abs(diff)
+
+    if (diffAbs < 1 || diffAbs > 3) return false
+    if (diff < 0) isIncreasing = false
+    if (diff > 0) isDecreasing = false
+
+    if (!isIncreasing && !isDecreasing) return false
+  }
+  return true
+}
+
+function a() {
+  let total = 0
+  for (const report of data) {
+    if (isSafe(report)) total++
+  }
+  console.log(total)
+}
+a()
+
+function b() {
+  let total = 0
+  for (const report of data) {
+    let safe = false
+    for (let i = 0; i < report.length; i++) {
+      const candidateReport = report.slice(0, i).concat(report.slice(i + 1))
+      if (isSafe(candidateReport)) {
+        safe = true
+        break
+      }
+    }
+    if (safe) total++
+  }
+  console.log(total)
+}
+b()
